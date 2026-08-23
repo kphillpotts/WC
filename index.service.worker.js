32c133c7a72c02e915d00ca5f886e148d7586ca1
Kym Phillpotts <kphillpotts@gmail.com>
2026-08-23 20:27:02 +1000

    New Release

--- index.service.worker.js
+++ index.service.worker.js
@@ -4,7 +4,7 @@
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
 /** @type {string} */
-const CACHE_VERSION = '1787479026|13005520069';
+const CACHE_VERSION = '1787480793|14772551290';
 /** @type {string} */
 const CACHE_PREFIX = 'WC-sw-cache-';
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
